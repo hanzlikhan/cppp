87a3cf943ba1e16f9d5d69ad19f3c79afa0b5e2e
hanzlikhan <khangormani79@gmail.com>
2024-04-19 19:22:59 +0500

    init

--- basics/hello.cpp
+++ basics/hello.cpp
@@ -1,13 +1,5 @@
 #include <iostream>
 using namespace std;
 int main(){
-    int a = 10;
-    for(int i= 1 ; i<a+1 ;i++){
-        cout<<a*i;
-        if (i != a){
-            cout<<"found";
-        }else{
-            cout<<"not found";
-        }
-    }
+    
 }
