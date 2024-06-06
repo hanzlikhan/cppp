7135e405b9374835bde35aaf8e64d26eb5312203
hanzlikhan <khangormani79@gmail.com>
2024-06-06 20:37:41 +0500

    init

--- classproblems/algo_4.3/Del_linear.cpp
+++ classproblems/algo_4.3/Del_linear.cpp
@@ -3,10 +3,13 @@ using namespace std;
 int main(){
     int n = 5;
     int LA [n] = {37,82,23,98,45};
-    int k = 1;
+    int k = 3;
     int item = LA[k];
-    for (int j = k; j <= n-1; j++)
-    {
+    // for (int j = k; j <= n-1; j++)
+    // {
+    //     LA[j] = LA[j+1];
+    // }
+    for (int j = k; j<n-1;j++){
         LA[j] = LA[j+1];
     }
     n = n-1;
